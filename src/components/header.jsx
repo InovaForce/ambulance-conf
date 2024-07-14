0ca4a1d8ec4049cffe59d02b94a7f222307708af
Sule Demir <sule.sozturk@hotmail.com>
2024-07-14 23:49:38 +0300

    updated

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,18 +1,22 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-
+import styles from '@/styles/components/header.module.scss';
+import { Nav } from 'react-bootstrap';
 
 const Header = () => {
   return (
-    <header>
-      <h1>Cretae your own Ambulance</h1>
-      <nav>
-        <ul>
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <Image src="/images/ıcu.png" alt="logo" width={120} height={40} />
+      </div>
+      <Nav className={styles.nav}>
+        <ul className={styles.nav_link}>
           <li><Link href="/">Home</Link></li>
           <li><Link href="/configurator">Configurator</Link></li>
           <li><Link href="/contact">Contact</Link></li>
         </ul>
-      </nav>
+      </Nav>
     </header>
   );
 };
