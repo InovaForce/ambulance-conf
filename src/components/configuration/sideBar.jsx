2836eb475fd75ccbe1c3e32fd340e2c4b0c17e18
ata324 <mavican324@gmail.com>
2024-07-14 17:44:21 +0200

    update

--- src/components/configuration/sideBar.jsx
+++ src/components/configuration/sideBar.jsx
@@ -3,8 +3,8 @@ import styles from '@/styles/components/sideBar.module.scss'
 const SideBar = () => {
   return (
     <div className={styles.sideBar}>
-      <h2>CONFIGURATION</h2>
-    
+      <h2 >Your Ambulance</h2>
+      <h4 className={styles.total}>Price Summary :</h4>
     </div>
   )
 }
