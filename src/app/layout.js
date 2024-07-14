c0e8744c037eaa6b5d8cb089b7c9c9ae344f5f76
ata324 <mavican324@gmail.com>
2024-07-14 14:55:22 +0200

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,8 @@ import { Inter } from "next/font/google";
 import "./globals.scss";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import { Col, Row } from "react-bootstrap";
+import SideBar from "@/components/configuration/sideBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,9 +15,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Header/>
-      <body className={inter.className}>{children}</body>
-      <Footer/>
+      <body className={`inter.className d-flex flex-column`} >
+        <Header />
+        <Row className="h-100">
+          <Col xs={8}>{children}</Col>
+          <Col xs={4}>
+            <SideBar />
+          </Col>
+        </Row>
+
+        <Footer />
+      </body>
     </html>
   );
 }
---
+++ src/components/configuration/sideBar.jsx
@@ -0,0 +1,11 @@
+
+import styles from './sideBar.module.scss'
+const SideBar = () => {
+  return (
+    <div className="sideBar">
+    
+    </div>
+  )
+}
+
+export default SideBar
