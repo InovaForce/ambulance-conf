28f171762857e45716f407caed941c3697b95a8c
Sule Demir <sule.sozturk@hotmail.com>
2024-07-20 23:46:25 +0300

    pages

--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,9 @@
 //api.js (to handle fetch requests)
+// api.ts (to handle fetch requests)
+const baseUrl= "https://669ab9c99ba098ed6100a050.mockapi.io/infinitychassis/";
+
+export const get = async (endpoint) => {
+    const response = await fetch(baseUrl + endpoint);
+    const data = await response.json();
+    return data;
+}
