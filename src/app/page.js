9d49132ad204c2d05fd2b45858d66d558a50325a
Tuba Gocmek <tubagocmek@gmail.com>
2024-07-21 14:18:19 +0300

    image

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 
+import ImageGallery from '@/components/ImageGallery';
 import React from 'react'
 
 const Home = () => {
   return (
     <div >
-      HOME
+     <ImageGallery/>
       
     </div>
   )
