04d5697bc699e77f76a225bb05964f08f91c60d5
Sule Demir <sule.sozturk@hotmail.com>
2024-07-23 20:39:53 +0300

    .

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,6 +16,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head >
+      <link rel="prefetch" href="http://localhost:3000/_next/static/css/app/configuration/page.css?v=1721753072477" />
+      <link rel="stylesheet" href="http://localhost:3000/_next/static/css/app/configuration/page.css?v=1721753072477" />
+      </head>   
       <body className={`inter.className d-flex flex-column`}>
         <Header />
         <Row className="flex-grow-1" style={{ Height: "100vh" }}>
