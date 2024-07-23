8bb91dca4d3d14b6985f731a1c4eef1929905acc
Tuba Gocmek <tubagocmek@gmail.com>
2024-07-24 01:20:25 +0300

    data1

--- src/components/configuration/fuel-type.jsx
+++ src/components/configuration/fuel-type.jsx
@@ -1,51 +1,76 @@
 "use client";
-import Image from 'next/image';
-import { useState } from 'react';
+import Image from "next/image";
+import { useState } from "react";
 
+const FuelTypeSelector = ({ setActive }) => {
+  const [selectedFuelType, setSelectedFuelType] = useState("");
 
-const FuelTypeSelector = ({setActive}) => {
-    const [selectedFuelType, setSelectedFuelType] = useState('');
+  const handleSelectFuelType = (type) => {
+    setSelectedFuelType(type);
+  };
 
-    const handleSelectFuelType = (type) => {
-        setSelectedFuelType(type);
-        setActive((prev) => prev + 1);
-    };
+  const handleNext = () => {
+    setActive((prev) => prev + 1);
+  };
 
-    return (
+  const handleBack = () => {
+    setActive((prev) => prev - 1);
+  };
+
+  return (
+    <div>
+      <h1>CHOOSE YOUR FUEL TYPE</h1>
+      <Image
+        src="/images/fuel_type.jpg"
+        width={800}
+        height={500}
+        alt="fuel_type"
+      />
+      <div style={{ display: "flex", justifyContent: "center", gap: "20px" }}>
         <div>
-            <h1>CHOOSE YOUR FUEL TYPE</h1>
-            <Image
-              src="/images/fuel_type.jpg"
-              width={800}
-              height={500}
-              alt="fuel_type"
-            />
-            <div style={{ display: 'flex', justifyContent: 'center', gap: '20px' }}>
-            <div>
-            <Image src="/images/fuel_type/gasoline.jpg"
-             width={300}
-            height={300}/>
-                <button onClick={() => handleSelectFuelType('Gasoline')} style={{ padding: '10px', fontSize: '16px', backgroundColor: selectedFuelType === 'Gasoline' ? 'green' : 'grey', color: 'white', border: 'none', borderRadius: '5px' }}>
-                    GASOLINE
-                </button>
-            </div>
-            <div className="fuelTypeImg" >
-            <Image  src="/images/fuel_type/diesel.jpg"
-             width={300}
-            height={300}
-            />
-                <button onClick={() => handleSelectFuelType('Diesel')} style={{ padding: '10px', fontSize: '16px', backgroundColor: selectedFuelType === 'Diesel' ? 'blue' : 'grey', color: 'white', border: 'none', borderRadius: '5px' }}>
-                    DIESEL
-                </button>
-             </div>
-            </div>
-            {selectedFuelType && (
-                <div style={{ marginTop: '20px', textAlign: 'center' }}>
-                    <h2>Selected Fuel Type: {selectedFuelType}</h2>
-                </div>
-            )}
+          <button
+            onClick={() => handleSelectFuelType("Gasoline")}
+            style={{
+              padding: "10px",
+              fontSize: "16px",
+              backgroundColor:
+                selectedFuelType === "Gasoline" ? "green" : "grey",
+              color: "white",
+              border: "none",
+              borderRadius: "5px",
+            }}
+          >
+            GASOLINE
+          </button>
         </div>
-    );
+        <div className="fuelTypeImg">
+          <button
+            onClick={() => handleSelectFuelType("Diesel")}
+            style={{
+              padding: "10px",
+              fontSize: "16px",
+              backgroundColor:
+                selectedFuelType === "Diesel" ? "blue" : "grey",
+              color: "white",
+              border: "none",
+              borderRadius: "5px",
+            }}
+          >
+            DIESEL
+          </button>
+        </div>
+      </div>
+      {selectedFuelType && (
+        <div style={{ marginTop: "20px", textAlign: "center" }}>
+          <h2>Selected Fuel Type: {selectedFuelType}</h2>
+        </div>
+      )}
+      <div style={{ display: "flex", justifyContent: "space-between", marginTop: "20px" }}>
+        <button className="back" onClick={handleBack}> Back </button>
+        <button className="next" onClick={handleNext}> Next </button>
+      </div>
+    </div>
+  );
 };
 
 export default FuelTypeSelector;
--- src/components/configuration/oxygen_system.jsx
+++ src/components/configuration/oxygen_system.jsx
@@ -1,10 +1,37 @@
 "use client";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { getAllInformation } from '@/services/api';
+import Image from 'next/image';
 
 const OxygenSystem = ({setActive}) => {
     const [selectedSystem, setSelectedSystem] = useState('Intersurgical');
     const [price, setPrice] = useState(100);
+    const [vehicleData, setVehicleData] = useState(null);
 
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const data = await getAllInformation();
+                setVehicleData(data);
+            } catch (error) {
+                console.error("Araç verilerini alırken hata oluştu:", error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    const handleNext= () =>{
+        setActive((prev) => prev + 1);
+    }
+
+    const handleBack= () =>{
+        setActive((prev) => prev - 1);
+    }
+    if (!vehicleData) {
+        return <div>Yükleniyor...</div>;
+    }
     const handleSelect = (system) => {
         setSelectedSystem(system);
         switch (system) {
@@ -20,21 +47,35 @@ const OxygenSystem = ({setActive}) => {
             default:
                 setPrice(0);
         }
-        setActive((prev) => prev + 1);
     };
 
     return (
         <div>
             <h1>Central Oxygen System Selector</h1>
+            <Image
+                    width={300}
+                    height={200} 
+                        src={vehicleData[8].image_url}
+                        alt={vehicleData} 
+                        style={{ objectFit: 'cover', borderRadius: '10px' }} 
+                    />
             <div>
-                <button onClick={() => handleSelect('Intersurgical')}>Intersurgical - $100</button>
-                <button onClick={() => handleSelect('Dräger')}>Dräger - $200</button>
-                <button onClick={() => handleSelect('Air Liquide')}>Air Liquide - $300</button>
+                <button key={vehicleData[8].name} 
+                        value={vehicleData[8].price}
+                        onClick={() => handleSelect('Intersurgical')}>Intersurgical - $100</button>
+                <button key={vehicleData[8].name} 
+                        value={vehicleData[8].price}
+                        onClick={() => handleSelect('Dräger')}>Dräger - $200</button>
+                <button key={vehicleData[8].name} 
+                        value={vehicleData[8].price}
+                        onClick={() => handleSelect('Air Liquide')}>Air Liquide - $300</button>
             </div>
             <div>
                 <h2>Selected System: {selectedSystem}</h2>
                 <h2>Price: ${price}</h2>
             </div>
+            <button onClick={handleBack}> Back </button>
+            <button onClick={handleNext}>Next </button>
         </div>
     );
 };
