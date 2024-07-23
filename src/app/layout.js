edb6dbe2201ab61ff4fe9490bc1eb2b6d0f5a8fd
ata324 <mavican324@gmail.com>
2024-07-24 01:44:38 +0200

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,7 +28,6 @@ export default function RootLayout({ children }) {
       <body className={`inter.className d-flex flex-column`}>
         <Header />
         {children}
-
         <Footer />
       </body>
     </html>
