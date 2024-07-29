bc32d7fefa87fa53cb0c806782e583ce5d272730
ata324 <mavican324@gmail.com>
2024-07-29 03:05:04 +0200

    update

--- src/app/[lang]/page.js
+++ src/app/[lang]/page.js
@@ -1,21 +1,16 @@
-import Header from '@/components/header';
+import Header from "@/components/header";
 import { getDictionary } from "@/services/dictionaries";
-import Language from '@/components/language';
+import HomeComp from "@/components/homeComp";
 
 const Home = async ({ params }) => {
-  
-
   const { lang } = params;
   const dict = await getDictionary(lang);
 
-  
-
   return (
-      <>
+    <>
       <Header lang={lang} />
-      <Language dict={dict} />
-      </>
-
+      <HomeComp dict={dict} />
+    </>
   );
 };
 
