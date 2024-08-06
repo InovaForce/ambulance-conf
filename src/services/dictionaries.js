faaf984219b9d3fb71e677611493b20105587e80
Sule Demir <sule.sozturk@hotmail.com>
2024-08-06 11:15:48 +0300

    contact comp corrected

--- src/services/dictionaries.js
+++ src/services/dictionaries.js
@@ -1,4 +1,29 @@
-import "server-only";
+/* import "server-only"; */
+
+const dictionaries = {
+  en: () => import("../dictionaries/en.json").then((module) => module.default),
+  es: () => import("../dictionaries/es.json").then((module) => module.default),
+  fr: () => import("../dictionaries/fr.json").then((module) => module.default),
+  ru: () => import("../dictionaries/ru.json").then((module) => module.default),
+  ar: () => import("../dictionaries/ar.json").then((module) => module.default),
+};
+
+export const getDictionary = async (locale = "en") => {
+  const loadDictionary = dictionaries[locale] || dictionaries["en"];
+  try {
+    return await loadDictionary();
+  } catch (error) {
+    console.error(`Error loading dictionary for locale "${locale}":`, error);
+    return dictionaries["en"](); // Varsayılan olarak İngilizce sözlüğü döndür
+  }
+};
+
+
+
+
+
+
+/* import "server-only";
 
 const dictionaries = {
   en: () => import("../dictionaries/en.json").then((module) => module.default),
@@ -12,3 +37,4 @@ export const getDictionary = async (locale = "en") => {
   const loadDictionary = dictionaries[locale] || dictionaries["en"];
   return loadDictionary();
 };
+ */
